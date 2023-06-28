1b703277e025bd8ecfe68e858c75a578779402c0
Guilherme Gazzo <guilhermegazzo@gmail.com>
2023-06-28 12:54:16 -0300

    improve prom and error handling

--- src/lib/prom.ts
+++ src/lib/prom.ts
@@ -40,10 +40,23 @@ export const openRoom = new client.Summary({
 	help: 'Open room sction',
 	labelNames: ['status'],
 });
+
 export const actions = new client.Summary({
 	name: 'rc_actions',
 	help: 'All performed actions',
 	labelNames: ['action', 'status'],
+});
+
+export const rest = new client.Summary({
+	name: 'rc_rest_time',
+	help: 'All performed rest actions',
+	labelNames: ['endpoint', 'status', 'method'],
+});
+
+export const subscriptions = new client.Summary({
+	name: 'rc_load_subscriptions',
+	help: 'Subscriptions',
+	labelNames: ['name', 'status'],
 });
 
 export const roleAdd = new client.Summary({
@@ -61,3 +74,49 @@ export const inquiryTaken = new client.Summary({
 export default client;
 
 export { client };
+
+const promWrapperBase =
+	<P extends client.Summary>(prom: P) =>
+	<F extends (...args: any[]) => Promise<any>>(action: string, fn: F): F => {
+		return (async (...args: any[]) => {
+			const endTimer = prom.startTimer({ action });
+			try {
+				const result = await fn(...args);
+				endTimer({ status: 'success' });
+				return result;
+			} catch (e) {
+				endTimer({ status: 'error' });
+				throw e;
+			}
+		}) as unknown as F;
+	};
+
+export const promWrapperAction = promWrapperBase(actions);
+
+export const promWrapperRest = <F extends (endpoint: string, ...args: any[]) => Promise<any>>(method: string, fn: F): F => {
+	return (async (endpoint: string, ...args: any[]) => {
+		const endTimer = rest.startTimer({ endpoint, method });
+		try {
+			const result = await fn(endpoint, ...args);
+			endTimer({ status: 'success' });
+			return result;
+		} catch (e) {
+			endTimer({ status: 'error' });
+			throw e;
+		}
+	}) as unknown as F;
+};
+
+export const promWrapperSubscribe = <F extends (...args: any[]) => Promise<any>>(fn: F): F => {
+	return (async (...args: any[]) => {
+		const endTimer = subscriptions.startTimer({ name: args.join('___') });
+		try {
+			const result = await fn(...args);
+			endTimer({ status: 'success' });
+			return result;
+		} catch (e) {
+			endTimer({ status: 'error' });
+			throw e;
+		}
+	}) as unknown as F;
+};
